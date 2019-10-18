d7b3c30c0c24d52610489391c8cde0da3987a5c7
btnz-k <github@elleryweber.com>
2019-10-17 23:37:22 -0700

    development of python script

--- hashlookup.py
+++ hashlookup.py
@@ -1,35 +1,27 @@
 #!/usr/bin/env python3
 
+# Include Modules
+import argparse
 
-""" hashlookup.py -  Script used to dynamically merge dit and pot file, searching and displaying user hashes or passwords """
-__author__  = "BTNZ"
-__version__ = "v1.0.2019.10.17"
-__status    = "Production"
+HelpText = ''' HASH LOOKUP SCRIPT'''
+#  Flags:
+#      -d, --dit <dit file>                  Path to NTDS DIT file
+#      -c, --cracked <cracked hash file>     Path to cracked hash file
+#      -u, user <username>                   Designates username/partial username to search for
+#      -o, --output <filename>               Merges the username and password and exports to <filename>
+#                                            in username:hash:password format
+#  Usage:
+#  ./hashlookup.py -d client.dit -h cracked_hashes.txt -u admin
+#        This will search for any usernames that contain the word "admin". All fields are required!
+#  ./hashlookup.py -d client.dit -h cracked_hashes.txt -o ./user_pass.txt
+#        This will merge the DIT and cracked hash file, in user:hash:pass format
 
 
 
-# INIT VARS, SET DEFAULT VALUES
-DITFILE=""
-HASHFILE=""
-USERNAME=""
-OUTFILE=""
-SPIN="-"
-HELP=' HASH LOOKUP SCRIPT
+# Initiate the parser
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--cracked", help="Path to cracked hash file")
+parser.add_argument("-d", "--dit", help="Path to dit/ntds file")
+parser.add_argument("-u", "--user", help="Username to search for")
+parser.parse_args()
 
-  Flags:
-      -?                      Display this help message
-      -d <dit file>           Path to NTDS DIT file
-      -c <cracked hash file>  Path to cracked hash file
-      -u <username>           Designates username/partial username to search for
-      -o <filename>           Merges the username and password and exports to <filename>
-                              in username:hash:password format
-
-  Usage:
-  ./hashlookup.sh -d client.dit -h cracked_hashes.txt -u admin
-        This will search for any usernames that contain the word "admin"
-        All fields are required!
-
-  ./hashlookup.sh -d client.dit -h cracked_hashes.txt -o ./user_pass.txt
-        This will merge the DIT and cracked hash file, in user:hash:pass format
-
-'
