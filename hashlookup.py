f422dac69d299a3c9b380a318752fa9430f6316e
btnz-k <github@elleryweber.com>
2019-10-18 00:32:23 -0700

    .

--- hashlookup.py
+++ hashlookup.py
@@ -2,26 +2,34 @@
 
 # Include Modules
 import argparse
-
-HelpText = ''' HASH LOOKUP SCRIPT'''
-#  Flags:
-#      -d, --dit <dit file>                  Path to NTDS DIT file
-#      -c, --cracked <cracked hash file>     Path to cracked hash file
-#      -u, user <username>                   Designates username/partial username to search for
-#      -o, --output <filename>               Merges the username and password and exports to <filename>
-#                                            in username:hash:password format
-#  Usage:
-#  ./hashlookup.py -d client.dit -h cracked_hashes.txt -u admin
-#        This will search for any usernames that contain the word "admin". All fields are required!
-#  ./hashlookup.py -d client.dit -h cracked_hashes.txt -o ./user_pass.txt
-#        This will merge the DIT and cracked hash file, in user:hash:pass format
-
-
+import sys
+import shutil
 
 # Initiate the parser
-parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--cracked", help="Path to cracked hash file")
-parser.add_argument("-d", "--dit", help="Path to dit/ntds file")
+parser = argparse.ArgumentParser(formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=shutil.get_terminal_size()[0]))
+parser.add_argument("DIT_File", help="Path to dit/ntds file")
+parser.add_argument("Cracked_File", help="Path to cracked hash file (hash:pass format)")
 parser.add_argument("-u", "--user", help="Username to search for")
-parser.parse_args()
+parser.add_argument("-o", "--output", help="Output file for merged document")
+parser.add_argument("-v","--verbose", help="Increase Verbosity", action="store_true")
+args = parser.parse_args()
+
+# Check required vars are present
+
+# Check file paths valid
+
+# Check for Output vs User
+
+# If output, merge and quit
+    # For merge, check for file exists and ask  for overwrite
+    # Copy dit to tmp for processing
+    # Strip computer accounts
+    # Loop through cracked hash file and modify /tmp dit
+    # Copy /tmp dit to correct path
+    # Quit script
+
+# Make sure user exists in DIT
+
+# Loop through found lines and print user:pass or user:hash
+
 
