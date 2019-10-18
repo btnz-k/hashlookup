231d4591972eb1987eb9d4bf597afac5862fa0cc
btnz-k <github@elleryweber.com>
2019-10-18 00:41:27 -0700

    .

--- hashlookup.py
+++ hashlookup.py
@@ -7,16 +7,29 @@ import shutil
 
 # Initiate the parser
 parser = argparse.ArgumentParser(formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=shutil.get_terminal_size()[0]))
-parser.add_argument("DIT_File", help="Path to dit/ntds file")
-parser.add_argument("Cracked_File", help="Path to cracked hash file (hash:pass format)")
+parser.add_argument("dit_file", help="Path to dit/ntds file")
+parser.add_argument("cracked_file", help="Path to cracked hash file (hash:pass format)")
 parser.add_argument("-u", "--user", help="Username to search for")
 parser.add_argument("-o", "--output", help="Output file for merged document")
 parser.add_argument("-v","--verbose", help="Increase Verbosity", action="store_true")
 args = parser.parse_args()
 
+if args.verbose:
+    print("Passed Args:")
+    print("\tDit File:      ", args.dit_file)
+    print("\tCracked File:  ", args.cracked_file)
+    print("\tUser:          ", args.user)
+    print("\tOutput File:   ", args.output)
+    print("\tVerbose:       ", args.verbose)
+
+
 # Check required vars are present
+if args.user is None and args.output is None:
+    print("Please pass a username to search for (-u or --user <username> ) or an output file to merge to (-o or --output <file>)")
+    quit()
 
 # Check file paths valid
+
 
 # Check for Output vs User
 
